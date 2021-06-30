9cc678b3655de6f5cefcf3b34207426d72030dad
aldopontoh <renaldopontoh@gmail.com>
2021-06-30 08:18:17 +0700

    add register test suite

---
+++ Test Suites/Pawoon Register/Register test suite.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Register test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3961916b-6372-40aa-b1a2-46d1aeb44c74</testSuiteGuid>
+   <testCaseLink>
+      <guid>e7a737cc-394f-42b9-ab66-4fc68c2a64a0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check completeness of register page (Daftar Akun)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d10fbbad-f690-4dbf-bce7-e34b1012704d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check completeness of register page (Ubah Nomor Page)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e9ed340c-1769-4db4-a80a-51843adf0d28</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check completeness register page (Informasi Bisnis)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6cdd84f6-4c92-4c6a-9877-7c339fcb26ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Registration E2E-Pawoon wih already registered email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c68f5dbb-cc07-4f16-b9b7-ef1d99b24782</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Registration E2E-Pawoon</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3a5255d-37fc-4727-9e0d-8032e40ef201</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify otp link</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
